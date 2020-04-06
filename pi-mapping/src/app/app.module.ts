07888aa0e5ef7d5a6410335ea463c3dfc036695e
Payam Dowlatyari <pdowlatyari@gmail.com>
2020-04-06 16:14:01 -0700

    add routing for about and addArticle

--- pi-mapping/src/app/app.module.ts
+++ pi-mapping/src/app/app.module.ts
@@ -2,22 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaperComponent } from './paper/paper.component';
 import { BrowserComponent } from './browser/browser.component';
+// import { AddArticleComponent } from './add-article/add-article.component';
+// import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PaperComponent,
-    BrowserComponent
+    BrowserComponent,
+    // AddArticleComponent,
+    // AboutComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
-  ],
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })

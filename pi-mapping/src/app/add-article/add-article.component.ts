0dfb1b458c214993cb8487c02fe990424f901e70
Payam Dowlatyari <pdowlatyari@gmail.com>
2020-04-29 18:41:06 -0700

    make search and sidebar sticky

--- pi-mapping/src/app/add-article/add-article.component.ts
+++ pi-mapping/src/app/add-article/add-article.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import * as bibtex from 'bibtex-parse-js';
+import { NgForm, FormGroup, FormBuilder, FormControl, FormArray, Validators } from '@angular/forms';
+
+
+var bib = bibtex.toJSON('@article{sample1,title={sample title}}');
 
 @Component({
   selector: 'app-add-article',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddArticleComponent implements OnInit {
 
+  input = {};
+  isValid = false;
+  //values = "";
+  // emails = "";
+  orderSheetForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    // bib.forEach(element => {
+    //   console.log(element);
+    // });
+    // console.log(this.email);
+
+
+
+  }
+  onKey(event: KeyboardEvent) { // with type info
+    // this.emails = (event.target as HTMLInputElement).value;
+    // this.input = event;
+  }
+  onSubmit(f: NgForm) {
+    // console.log(f.value);
+    // console.log(f.valid);
+    this.input = f.value;
+    console.log(this.input);
+    this.isValid = f.valid;
+    console.log(this.isValid);
+
+
+  }
+  onResetForm() {
+    this.orderSheetForm.reset();
   }
 
 }

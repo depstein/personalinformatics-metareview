840c49f0efe12b138b1140a784d1b8a9ff07ef1f
Simran Ahuja <simran8072@gmail.com>
2020-05-20 16:29:14 -0700

    read&parse updated
    Getting errors.

--- pi-mapping/src/app/add-article/add-article.component.ts
+++ pi-mapping/src/app/add-article/add-article.component.ts
@@ -9,36 +9,60 @@ import * as bibtex from 'bibtex-parse-js';
 export class AddArticleComponent implements OnInit {
 
   bibtexFileString: string = '';
-  viewConvertedFile: any;
+  viewJSONfile: any;
+
+  entryTags: any;
+
+  title: string;
+  authors: any[];
+  year: string;
+  booktitle: string;
+  url: string;
+
+
 
   constructor() {
     // var bibtexParse= require('bibtex-parse-js');
-    this.viewConvertedFile= bibtex.toJSON(this.bibtexFileString);
-    console.log(this.viewConvertedFile); //to view the JSON array in the console
+    this.title= '';
+    this.authors= [];
+    this.year= '';
+    this.booktitle= '';
+    this.url= '';
 
-    // this.viewConvertedFile= bibtex.toJSON(this.bibtexFileString);
-    // console.log(this.viewConvertedFile);
+
+    console.log(typeof(this.viewJSONfile));
+    console.log(this.viewJSONfile); //to view the JSON array in the console
+
 
    }
 
   ngOnInit() {
   }
 
-  public readFile(fileList: FileList): void {
+  public readAndParseFile(fileList: FileList): void {
     let file = fileList[0];
     let fileReader = new FileReader();
     fileReader.onloadend = (x) => {
+
       this.bibtexFileString = fileReader.result.toString();
 
-      // this.viewConvertedFile= bibtex.toJSON(this.bibtexFileString);
-      // console.log(this.viewConvertedFile); //to view the JSON array in the console
+      // console.log(this.bibtexFileString);
+      this.viewJSONfile= bibtex.toJSON(this.bibtexFileString);
+      // console.log(this.bibtexFileString);
+      // console.log(this.viewJSONfile);
 
-      console.log(this.bibtexFileString);
-      this.viewConvertedFile= bibtex.toJSON(this.bibtexFileString);
-      console.log(this.viewConvertedFile);
+      this.entryTags = this.viewJSONfile[0].entryTags;
+      // console.log(this.entryTags);
+      // this.title = this.entryTags.title;
+      // this.authors = this.entryTags.authors[0];
+      // this.year = this.entryTags.year;
+      // this.booktitle = this.entryTags.booktitle;
+      // this.url = this.entryTags.url;
     }
+
     fileReader.readAsText(file);
   }
+
 
 
 }
